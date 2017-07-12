cbbdee0604e41a47c6c29b5f838c95ec96087a32
dr5801 <dr5801@cs.ship.edu>
2017-07-11 21:26:59 -0400

    made changes

--- Gym-Data-Project/src/data_source/RowDataGateway.java
+++ Gym-Data-Project/src/data_source/RowDataGateway.java
@@ -31,13 +31,15 @@ public interface RowDataGateway
 	/**
 	 * Deletes a row based on the date 
 	 * @param date
+	 * @throws SQLException 
 	 */
-	public void deleteRow(Date date);
+	public void deleteRow(Date date) throws SQLException;
 	
 	/**
 	 * Retrieves a row based on the date
 	 * @param date
 	 * @return TripDTO containing all of the information from that row
+	 * @throws SQLException 
 	 */
-	public TripDTO retrieveRow(Date date);
+	public TripDTO retrieveRow(Date date) throws SQLException;
 }
